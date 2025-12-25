9ca5ac49c3b844e7d372751f6d97bc153ccc050b
Bearofbusiness <dev@bearof.business>
2025-12-24 22:50:41 -0500

    Change return type of loadPlayerData method to HashMap for to allow for mutation

--- src/main/java/org/fracturedsmp/finality/data/YamlHandler.java
+++ src/main/java/org/fracturedsmp/finality/data/YamlHandler.java
@@ -4,6 +4,7 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.fracturedsmp.finality.util.config.ConfigFile;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class YamlHandler {
@@ -18,17 +19,17 @@ public class YamlHandler {
         this.playerDataFile.load();
     }
 
-    public Map<String, PlayerData> loadPlayerData() {
+    public HashMap<String, PlayerData> loadPlayerData() {
         ConfigurationSection playerData = playerDataFile.getConfigurationSection("playerdata");
         if(playerData != null) {
-            return playerData.getValues(false).entrySet().stream().collect(
+            return (HashMap<String, PlayerData>) playerData.getValues(false).entrySet().stream().collect(
                     java.util.stream.Collectors.toMap(
                             Map.Entry::getKey,
                             entry -> new PlayerData((ConfigurationSection) entry.getValue())
                     )
             );
         }
-        return Map.of();
+        return new HashMap<>();
     }
 
     public void savePlayerData(Map<String, PlayerData> playerData) {
